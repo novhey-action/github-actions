8a5c996f7966451ff88ae8e87aeb3e45b8b45b73
hyeju1123 <hjjun1123@gmail.com>
2024-10-19 09:01:24 +0900

    Add app.yaml

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi
+          Learn GithubAction cicd
         </a>
       </header>
     </div>
